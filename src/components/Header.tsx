f8563c61ac5dd88c6883bf4a8dff8e31bb9832a5
Xori <xori93@aol.com>
2026-03-03 10:14:19 -0500

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ type HeaderProp = {
   button1: string
   button2: string
   img: string
-  italic:string
-  bold: string
-  img2: string
+  italic?:string
+  bold?: string
+  img2?: string
 }
 
 const Header = ( {title, description, button1, button2, img, italic, bold, img2}: HeaderProp ) => {
@@ -19,12 +19,12 @@ const Header = ( {title, description, button1, button2, img, italic, bold, img2}
       style={{ backgroundImage: `url(${img})` }}
     >
       <h2 className="text-8xl font-bold tracking-tight">
-        <span className="font-bold">{bold}</span> {title} <i className="text-blue-200 font-cursive">{italic}</i>
+       {bold && <span className="font-bold">{bold}</span>} {title} {italic && <i className="text-blue-200 font-cursive">{italic}</i>}
        
       </h2>
 
       <div className="flex items-center gap-1">
-        <img src={img2} alt="" className="w-5 h-5" />
+        {img2 && <img src={img2} alt="" className="w-5 h-5" /> }
         <h3 className="text-4xl">
           {description}
         </h3>
